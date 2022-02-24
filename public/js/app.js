bd799f748c0a16877cbc49ccf9b02a6c2620e832
Tudor Crișan <71275739+tudorcrisan1231@users.noreply.github.com>
2022-02-24 19:15:15 +0200

    laptop commit

--- public/js/app.js
+++ public/js/app.js
@@ -19809,7 +19809,7 @@ process.umask = function() { return 0; };
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkatestat_tft_laravel"] = self["webpackChunkatestat_tft_laravel"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
