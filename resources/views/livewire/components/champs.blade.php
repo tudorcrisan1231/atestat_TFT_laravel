5fa160095915f01750940df7784748c625665ddc
crisantudor79@yahoo.com <crisantudor79@yahoo.com>
2022-03-06 19:10:54 +0200

    champs items and level, quick_stats + tooltip

--- resources/views/livewire/components/champs.blade.php
+++ resources/views/livewire/components/champs.blade.php
@@ -1,14 +1,72 @@
 <div class="game_champs">
 
+    @php
+        $items = [];
+        for ($i = 0; $i < count($match['info']['participants'][$mainPlayerPOZ]['units']); $i++) {
+            $aux = [];
+            for ($j = 0; $j < count($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['items']); $j++) {
+                for ($h = 0; $h < count($augments_itemsJSON); $h++) {
+                    if ($augments_itemsJSON[$h]['id'] == $match['info']['participants'][$mainPlayerPOZ]['units'][$i]['items'][$j]) {
+                        // echo $augments_itemsJSON[$h]['name'];
+                        array_push($aux, [$augments_itemsJSON[$h]['name'], $augments_itemsJSON[$h]['loadoutsIcon']]);
+                    }
+                }
+            }
+            if (count($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['items']) == 0) {
+                array_push($items, []);
+            } else {
+                array_push($items, $aux);
+            }
+        }
+        
+        // dd($items);
+        
+        function starColor($tier)
+        {
+            if ($tier == 3) {
+                return 'gold';
+            } elseif ($tier == 2) {
+                return 'silver';
+            } else {
+                return '#8B4513';
+            }
+        }
+        
+    @endphp
 
     @for ($i = 0; $i < count($match['info']['participants'][$mainPlayerPOZ]['units']); $i++)
-        <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}/hud/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}_square.tft_set6.png"
-            alt="" class="game_champs_img tippy_tooltip"
-            onerror="this.onerror=null;this.src='https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}/hud/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}.tft_set6_stage2.png';"
-            data-tippy-content="{{ substr($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id'], 5) }}"
-            onmouseover="startTippy()">
+        <div class="game_champs_champ">
+            <div class="game_champs_level">
+                @for ($j = 0; $j < $match['info']['participants'][$mainPlayerPOZ]['units'][$i]['tier']; $j++)
+                    <svg class="game_champs_level_svg" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img"
+                        width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                        <path
+                            fill="{{ starColor($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['tier']) }}"
+                            d="m16 2l-4.55 9.22l-10.17 1.47l7.36 7.18L6.9 30l9.1-4.78L25.1 30l-1.74-10.13l7.36-7.17l-10.17-1.48Z" />
+                    </svg>
+                @endfor
 
-        {{-- sunt 2 tipuri de png uri cu campionii din tft, una cu campionii simpli si una cu campionii din stage2, pun functia onerror, care daca nu mi randeaza prima img, mi o pune a pe a doua cu stage2 --}}
+            </div>
+
+            <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}/hud/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}_square.tft_set6.png"
+                alt="" class="game_champs_img tippy_tooltip"
+                onerror="this.onerror=null;this.src='https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}/hud/{{ strtolower($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id']) }}.tft_set6_stage2.png';"
+                data-tippy-content="{{ substr($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['character_id'], 5) }}"
+                onmouseover="startTippy()">
+            {{-- sunt 2 tipuri de png uri cu campionii din tft, una cu campionii simpli si una cu campionii din stage2, pun functia onerror, care daca nu mi randeaza prima img, mi o pune a pe a doua cu stage2 --}}
+
+            <div class="game_champs_items">
+                @for ($j = 0; $j < count($items[$i]); $j++)
+                    @if (count($items[$i][$j]) != 0)
+                        <img class="game_champs_items_img tippy_tooltip"
+                            src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/maps/particles/tft/{{ strtolower(substr($items[$i][$j][1], 48)) }}"
+                            alt="items icon tft" data-tippy-content="{{ $items[$i][$j][0] }}"
+                            onmouseover="startTippy()">
+                    @endif
+                @endfor
+            </div>
+        </div>
     @endfor
 
 </div>
+{{-- onerror="this.onerror=null;this.src='https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/maps/particles/tft/tft_item_unknown.png'" --}}
--- resources/views/livewire/components/quick_stats.blade.php
+++ resources/views/livewire/components/quick_stats.blade.php
@@ -1,3 +1,41 @@
+@php
+$time_alive = gmdate('i:s', $match['info']['participants'][$mainPlayerPOZ]['time_eliminated']);
+
+$board_value = 0;
+
+//1 campioni ca sa l fac de 1
+//3 campioni ca sa l fac de 2
+//9 campioni ca sa l fac de 3
+
+for ($i = 0; $i < count($match['info']['participants'][$mainPlayerPOZ]['units']); $i++) {
+    if ($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['tier'] == 1) {
+        $board_value += ($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['rarity'] + 1) * 1;
+    } elseif ($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['tier'] == 2) {
+        $board_value += ($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['rarity'] + 1) * 3;
+    } elseif ($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['tier'] == 3) {
+        $board_value += ($match['info']['participants'][$mainPlayerPOZ]['units'][$i]['rarity'] + 1) * 9;
+    }
+}
+@endphp
+
 <div class="game_stats">
-    <p>quick stats</p>
+    <div class="game_stats_item tippy_tooltip" data-tippy-content="Total damage dealt to players"
+        onmouseover="startTippy()">
+        <img src="https://raw.communitydragon.org/pbe/game/assets/ux/tft/stageicons/announce_icon_combat.png"
+            alt="comabt">
+        <p>{{ $match['info']['participants'][$mainPlayerPOZ]['total_damage_to_players'] }}</p>
+    </div>
+    <div class="game_stats_item tippy_tooltip" data-tippy-content="Time alive" onmouseover="startTippy()">
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em"
+            preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+            <path fill="currentColor"
+                d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8zm.5-13H11v6l5.25 3.15l.75-1.23l-4.5-2.67z" />
+        </svg>
+        <p>{{ $time_alive }}</p>
+    </div>
+    <div class="game_stats_item tippy_tooltip" data-tippy-content="Total board value" onmouseover="startTippy()">
+        <img src="https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-career-stats/global/default/category_income.png"
+            alt="comabt">
+        <p>{{ $board_value }}</p>
+    </div>
 </div>
