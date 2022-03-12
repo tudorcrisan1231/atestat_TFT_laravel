e9286afefeea7e62f014fce3d23d63ae2cb020ff
crisantudor79@yahoo.com <crisantudor79@yahoo.com>
2022-03-12 19:28:19 +0200

    advanced stats done
    -traits
    -units

--- resources/views/livewire/components/advanced_data.blade.php
+++ resources/views/livewire/components/advanced_data.blade.php
@@ -19,7 +19,7 @@
         
         $orderedSummoners = orderPlacemnets($match['info']['participants']); //vector cu participantii ordonati crescator dupa locul obtinut
         
-        $companionsIMG = [];
+        $companionsIMG = []; //vector cu toate skin urile
         for ($j = 0; $j < count($orderedSummoners); $j++) {
             for ($i = 0; $i < count($companionJSON); $i++) {
                 if ($orderedSummoners[$j]['companion']['content_ID'] == $companionJSON[$i]['contentId']) {
@@ -28,7 +28,7 @@
             }
         }
         
-        $augmentsSummoners = [];
+        $augmentsSummoners = []; //vector cu toate augmentele jucatorilor
         for ($h = 0; $h < count($orderedSummoners); $h++) {
             $help = [];
             for ($i = 0; $i < count($augments_itemsJSON); $i++) {
@@ -42,15 +42,45 @@
             array_push($augmentsSummoners, $help);
         }
         
-        // dd($augmentsSummoners);
+        $traitsSummoners = []; //vector cu toate trait urile jucatorilor
+        for ($i = 0; $i < count($orderedSummoners); $i++) {
+            $traitsSingleSummoner = orderTierTraits($orderedSummoners[$i]['traits']);
+        
+            array_push($traitsSummoners, $traitsSingleSummoner);
+        }
+        
+        $itemsSummoners = []; //vector cu toti campionii jucatorilor + iteme + level
+        
+        for ($h = 0; $h < count($orderedSummoners); $h++) {
+            $itemsSingleSummoner = [];
+            for ($i = 0; $i < count($orderedSummoners[$h]['units']); $i++) {
+                $aux = [];
+                for ($j = 0; $j < count($orderedSummoners[$h]['units'][$i]['items']); $j++) {
+                    for ($g = 0; $g < count($augments_itemsJSON); $g++) {
+                        if ($augments_itemsJSON[$g]['id'] == $orderedSummoners[$h]['units'][$i]['items'][$j]) {
+                            // echo $augments_itemsJSON[$h]['name'];
+                            array_push($aux, [$augments_itemsJSON[$g]['name'], $augments_itemsJSON[$g]['loadoutsIcon']]);
+                        }
+                    }
+                }
+                if (count($orderedSummoners[$h]['units'][$i]['items']) == 0) {
+                    array_push($itemsSingleSummoner, []);
+                } else {
+                    array_push($itemsSingleSummoner, $aux);
+                }
+            }
+            array_push($itemsSummoners, $itemsSingleSummoner);
+        }
+        
+        // dd($itemsSummoners);
         
     @endphp
 
-    <table style="width: 100%" class="advanced_table">
+    <table style="width: 100%; overflow-x:auto;" class="advanced_table">
         <tr style="background-color: rgba(0, 0, 0, 0.1);">
             <th>Rank</th>
             <th>Summoner</th>
-            <th>Damage done</th>
+            <th>Damage <br> done</th>
             <th>Alive</th>
             <th>Augments</th>
             <th>Traits</th>
@@ -60,15 +90,17 @@
         @for ($i = 0; $i < count($summonerNames); $i++)
             <tr>
                 <td>#{{ $i + 1 }}</td>
-                <td class="advanced_table_profile">
-                    <img class="game_companionIMG"
-                        src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/companions/{{ strtolower($companionsIMG[$i]) }}"
-                        alt="companion img">
-                    <p class="advanced_table_profile_level tippy_tooltip" data-tippy-content="Level"
-                        onmouseover="startTippy()"> {{ $orderedSummoners[$i]['level'] }}</p>
-                    <a target="_blank" href="/{{ $region }}/{{ $summonerNames[$i]['name'] }}">
-                        {{ $summonerNames[$i]['name'] }}
-                    </a>
+                <td class="advanced_table_profile_td">
+                    <div class="advanced_table_profile">
+                        <img class="game_companionIMG"
+                            src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/companions/{{ strtolower($companionsIMG[$i]) }}"
+                            alt="companion img">
+                        <p class="advanced_table_profile_level tippy_tooltip" data-tippy-content="Level"
+                            onmouseover="startTippy()"> {{ $orderedSummoners[$i]['level'] }}</p>
+                        <a target="_blank" href="/{{ $region }}/{{ $summonerNames[$i]['name'] }}">
+                            {{ $summonerNames[$i]['name'] }}
+                        </a>
+                    </div>
                 </td>
                 <td>{{ $orderedSummoners[$i]['total_damage_to_players'] }}</td>
                 <td>{{ gmdate('i:s', $orderedSummoners[$i]['time_eliminated']) }}</td>
@@ -79,8 +111,102 @@
                             data-tippy-content="{{ $augmentsSummoners[$i][$j][1] }}" onmouseover="startTippy()">
                     @endfor
                 </td>
-                <td>traits</td>
-                <td>campioni</td>
+                <td>
+                    <div class="advanced_table_traits">
+                        @for ($j = 0; $j < count($traitsSummoners[$i]); $j++)
+                            @if ($traitsSummoners[$i][$j]['tier_current'] > 0)
+                                <div class="game_traits_trait">
+
+
+                                    <div class="game_traits_trait_img">
+                                        {{-- unde am if uri sunt exceptii, din documentatia ddragon si din requesturile de pe api --}}
+
+
+                                        @if ($traitsSummoners[$i][$j]['name'] == 'Set6_Hextech')
+                                            <img src="https://raw.communitydragon.org/latest/game/assets/ux/traiticons/trait_icon_hextech.png"
+                                                alt="" class="game_traits_trait_img_TRAIT tippy_tooltip"
+                                                style="background-image: url('/images/tier{{ $traitsSummoners[$i][$j]['style'] }}.png'); "
+                                                data-tippy-content="{{ $traitsSummoners[$i][$j]['num_units'] }} Hextech"
+                                                onmouseover="startTippy()">
+                                        @elseif($traitsSummoners[$i][$j]['name'] == 'Set6_Bodyguard')
+                                            <img src="https://raw.communitydragon.org/latest/game/assets/ux/traiticons/trait_icon_6_hero.png"
+                                                alt="" class="game_traits_trait_img_TRAIT tippy_tooltip"
+                                                style="background-image: url('/images/tier{{ $traitsSummoners[$i][$j]['style'] }}.png'); "
+                                                data-tippy-content="{{ $traitsSummoners[$i][$j]['num_units'] }} Bodyguard"
+                                                onmouseover="startTippy()">
+                                        @elseif(strtolower(substr($traitsSummoners[$i][$j]['name'], 3)) == '6_mutant')
+                                            <img src="https://raw.communitydragon.org/latest/game/assets/ux/traiticons/trait_icon_6_experimental.png"
+                                                alt="" class="game_traits_trait_img_TRAIT tippy_tooltip"
+                                                style="background-image: url('/images/tier{{ $traitsSummoners[$i][$j]['style'] }}.png'); "
+                                                data-tippy-content="{{ $traitsSummoners[$i][$j]['num_units'] }} Mutant"
+                                                onmouseover="startTippy()">
+                                        @else
+                                            <img src="https://raw.communitydragon.org/latest/game/assets/ux/traiticons/trait_icon_{{ strtolower(substr($traitsSummoners[$i][$j]['name'], 3)) }}.png"
+                                                alt="" class="game_traits_trait_img_TRAIT tippy_tooltip"
+                                                style="background-image: url('/images/tier{{ $traitsSummoners[$i][$j]['style'] }}.png'); "
+                                                data-tippy-content="{{ $traitsSummoners[$i][$j]['num_units'] }} {{ ucfirst(strtolower(substr($traitsSummoners[$i][$j]['name'], 5))) }}"
+                                                onmouseover="startTippy()">
+                                        @endif
+
+
+                                    </div>
+
+
+                                </div>
+                            @endif
+                        @endfor
+                    </div>
+
+                </td>
+                <td>
+                    <div class="advanced_table_units">
+                        @for ($j = 0; $j < count($orderedSummoners[$i]['units']); $j++)
+
+                            <div class="advanced_table_units_champ">
+                                <div class="game_champs_level">
+                                    @for ($h = 0; $h < $orderedSummoners[$i]['units'][$j]['tier']; $h++)
+                                        <svg class="game_champs_level_svg" xmlns="http://www.w3.org/2000/svg"
+                                            aria-hidden="true" role="img" width="1em" height="1em"
+                                            preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+                                            <path fill="{{ starColor($orderedSummoners[$i]['units'][$j]['tier']) }}"
+                                                d="m16 2l-4.55 9.22l-10.17 1.47l7.36 7.18L6.9 30l9.1-4.78L25.1 30l-1.74-10.13l7.36-7.17l-10.17-1.48Z" />
+                                        </svg>
+                                    @endfor
+
+                                </div>
+
+                                @if (substr($orderedSummoners[$i]['units'][$j]['character_id'], 5) == '_Vi')
+                                    <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/tft6_vi/hud/tft6_vi.tft_set6_stage2.png"
+                                        alt="vi" class="game_champs_img tippy_tooltip"
+                                        data-tippy-content="{{ substr($orderedSummoners[$i]['units'][$j]['character_id'], 6) }}"
+                                        onmouseover="startTippy()">
+
+                                    {{-- la campionul VI, apare o eroare in documnetatie si trebuie pus manual --}}
+                                @else
+                                    <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{{ strtolower($orderedSummoners[$i]['units'][$j]['character_id']) }}/hud/{{ strtolower($orderedSummoners[$i]['units'][$j]['character_id']) }}.tft_set6_stage2.png"
+                                        alt="" class="game_champs_img tippy_tooltip"
+                                        onerror="this.src='https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/{{ strtolower($orderedSummoners[$i]['units'][$j]['character_id']) }}/hud/{{ strtolower($orderedSummoners[$i]['units'][$j]['character_id']) }}_square.tft_set6.png';"
+                                        data-tippy-content="{{ substr($orderedSummoners[$i]['units'][$j]['character_id'], 5) }}"
+                                        onmouseover="startTippy()">
+                                    {{-- sunt 2 tipuri de png uri cu campionii din tft, una cu campionii simpli si una cu campionii din stage2, pun functia onerror, care daca nu mi randeaza prima img, mi o pune a pe a doua cu stage2 --}}
+                                @endif
+
+                                <div class="game_champs_items">
+                                    @for ($h = 0; $h < count($itemsSummoners[$i][$j]); $h++)
+                                        @if (count($itemsSummoners[$i][$j][$h]) != 0)
+                                            <img class="game_champs_items_img tippy_tooltip"
+                                                src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/maps/particles/tft/{{ strtolower(substr($itemsSummoners[$i][$j][$h][1], 48)) }}"
+                                                alt="items icon tft"
+                                                data-tippy-content="{{ $itemsSummoners[$i][$j][$h][0] }}"
+                                                onmouseover="startTippy()">
+                                        @endif
+                                    @endfor
+                                </div>
+                            </div>
+
+                        @endfor
+                    </div>
+                </td>
                 <td>{{ $orderedSummoners[$i]['gold_left'] }}</td>
             </tr>
         @endfor
